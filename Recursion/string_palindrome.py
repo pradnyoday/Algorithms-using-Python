a6501035c9cbf6798372b8a4e981cb322153cb38
Pradnyoday Mirajkar <pradnyoday@gmail.com>
2021-10-14 13:29:46 +0530

    added string palindrome recursive

---
+++ Recursion/string_palindrome.py
@@ -0,0 +1,10 @@
+def isPalindrome(s, l, r):
+    if(l >= r): return True
+    if(s[l] != s[r]): return False
+    return isPalindrome(s, l + 1, r - 1)
+s = "pradnyoday"
+n = 10
+s1 = "madam"
+n1 = 5
+print(isPalindrome(s, 0, n-1))
+print(isPalindrome(s1, 0, n1-1))
