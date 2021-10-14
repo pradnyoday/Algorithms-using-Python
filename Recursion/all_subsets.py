e98769f8bc2c6ebe17b9a7abc14316542df4b5ae
Pradnyoday Mirajkar <pradnyoday@gmail.com>
2021-10-14 17:22:25 +0530

    added all subset problem

---
+++ Recursion/all_subsets.py
@@ -0,0 +1,11 @@
+def print_subsets(i, ans, arr, n):
+    if(i == n): 
+        print(ans)
+        return
+    ans.append(arr[i]) # Pick item
+    print_subsets(i+1, ans, arr, n)
+    ans.pop() # Dont Pick item
+    print_subsets(i+1, ans, arr, n)
+arr = [1, 2, 3]
+n = 3
+print_subsets(0, [], arr, n)
